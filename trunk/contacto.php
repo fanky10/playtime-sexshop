b5564c1e21abb08c3be5a670286c5d03c2be0357
infomawape@gmail.com <infomawape@gmail.com@a2532c14-442b-bbd0-053e-8828371a10ae>
2011-02-20 04:55:40 +0000

    arregle el script de contacto para que solo lo envie una vez

--- trunk/contacto.php
+++ trunk/contacto.php
@@ -12,7 +12,7 @@
 
 		<!--JS FILES-->
         <script src="js/jquery-1.5.js" type="text/javascript"></script>
-		<!--<!--<script src="js/action.js" type="text/javascript"></script>-->--><!--PROPIO-->
+		<script src="js/secciones/contact.js" type="text/javascript"></script><!--PROPIO-->
         
 		<script src="js/jquery.validate.js" type="text/javascript"></script><!--EMAIL-->
         <meta http-equiv="content-type" content="text/html;charset=iso-8859-1" />
@@ -21,58 +21,6 @@
         <!--[if lte IE 7]>
         <script type="text/javascript" src="js/supersleight-min.js"></script>
         <![endif]-->
-        
-        <script type="text/javascript">
-		// JavaScript Document
-$().ready(function(){
-/*******************************************************
-*					ANCHO COLUMNA					   *
-********************************************************/
-var alturaMax;
-//Obtiene la altura máxima de las 2 columnas y almacena el valor en AlturaMax
-AlturaMax = Math.max( $("#lateral_izquierdo").height(), $("div.contenido").height() );
-//Asigna la propiedad height con el valor de AlturaMax
-$("#lateral_izquierdo").height(AlturaMax);
-//Asigna la propiedad height con el valor de AlturaMax
-$("div.contenido").height(AlturaMax);
-/*******************************************************
-*			VALIDAR FORMULARIO CONTACTO				   *
-********************************************************/
-$("#frmContact").validate({
-	event: "blur",
-	rules: {
-	'data[Contact][name]': "required",
-	'data[Contact][recipient]': { required: true, email: true },
-	'data[Contact][message]': "required"
-	},
-	messages: {
-	'data[Contact][name]': "Por favor ingrese su nombre",
-	'data[Contact][recipient]': "Ingrese una dirección de e-mail válida",
-	'data[Contact][message]': "Por favor, ingrese su mensaje o consulta"
-	},
-	
-	debug: true,
-	errorElement: "label",
-	errorContainer: $("#errores"),
-	submitHandler: function(form){
-	
-	$.ajax({
-	
-		type: "GET",
-		url:"envio.php",
-		contentType: "application/x-www-form-urlencoded",
-		processData: false,
-		data: "nombre="+$('#ContactName').val()+"&email="+$('#ContactRecipient').val()+"&telefono="+$('#ContactPhone').val()+"&comentario="+$('#ContactMessage').val(),
-		success: function(msg){
-		//if(msg==1){
-		$("#mensaje").html("<strong>El mensaje se ha enviado correctamente!</strong>");
-		//}
-		}
-	});
-	}
-});
-});
-</script>
         
     </head>
     
