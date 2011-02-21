510f97b2147099af3782338d5d1d3d4b03b8e65c
infomawape@gmail.com <infomawape@gmail.com@a2532c14-442b-bbd0-053e-8828371a10ae>
2011-02-21 03:23:19 +0000

    agregue el if dentro de las categorias, ya que le faltaba una clase al primer item y ademas arregle un error de css que habia cuando se colocaba la etiqueta <li class=""></li>

--- trunk/menu_categorias.php
+++ trunk/menu_categorias.php
@@ -9,10 +9,15 @@
                 //TODO: change href
                 for($index=0;$index < $found;$index++){
                     $oCategoria = $catList[$index];
-
+					if($index==0)
+					{
+						echo "<li class=\"item first\"><a href=\"tienda.php?id_cat=".$oCategoria->getId_Categoria()."\"> ".$oCategoria->getNombre()."</a></li>";
+					}
+					else
+					{
                     //escribo el nombre de la categoria
                     //TODO agregar jscript para llamar a productos
-                    echo "<li class\"item\"><a href=\"tienda.php?id_cat=".$oCategoria->getId_Categoria()."\"> ".$oCategoria->getNombre()."</a></li>";
+                    echo "<li class=\"item\"><a href=\"tienda.php?id_cat=".$oCategoria->getId_Categoria()."\"> ".$oCategoria->getNombre()."</a></li>";		}
                 }
             }catch(Exception $ex){
                 echo 'Ha ocurrido una exception: '.$ex->getMessage();
