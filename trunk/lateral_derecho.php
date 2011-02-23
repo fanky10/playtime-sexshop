2342c0361e5584c1fc625bd1cd10eefe4b48250b
lisandrovico@gmail.com <lisandrovico@gmail.com@a2532c14-442b-bbd0-053e-8828371a10ae>
2011-02-23 16:44:44 +0000

    arreglados bugs en el detalle de producto

--- trunk/lateral_derecho.php
+++ trunk/lateral_derecho.php
@@ -1,8 +1,8 @@
 <p class="social">
     <span>Sueguinos:</span>
-    <a href="http://www.facebook.com/update_security_info.php?wizard=1#!/pages/Play-Time/10150131130005716" class="socialFacebook"></a>
-    <a href="http://twitter.com/playtimesexshop" target="_blank" class="socialTwitter"></a>
-    <a href="#" class="socialFeed"></a>
+    <a href="http://www.facebook.com/update_security_info.php?wizard=1#!/pages/Play-Time/10150131130005716" title="Facebook" target="_blank" class="socialFacebook"></a>
+    <a href="http://twitter.com/playtimesexshop" title="Twitter" target="_blank" class="socialTwitter"></a>
+    <a href="#" title="RSS" class="socialFeed"></a>
 </p>
 <a class="banner" href="#" title="Conectate al MSN">
     <img src="images/banners/bnr-msn03.jpg" alt="Conectate al MSN" />
