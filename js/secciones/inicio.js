c9fa60a6772940e2b21c232a1f53c37d7de55338
lisandrovico <lisandrovico@gmail.com>
2011-02-23 22:31:55 +0000

    index.php: Slider y productos destacados

--- js/secciones/inicio.js
+++ js/secciones/inicio.js
@@ -15,38 +15,20 @@ $("div.contenido").height(AlturaMax);
 ********************************************************/
 
 $('#slider1').anythingSlider({
-			startStopped    : false, // If autoPlay is on, this can force it to start stopped
-			width           : 550,   
-			height          : 200,   
-			theme           : 'metallic',
-			autoPlayLocked  : true,  // If true, user changing slides will not stop the slideshow
-			resumeDelay     : 10000, // Resume slideshow after user interaction, only if autoplayLocked is true (in milliseconds).
-			
-			startPanel          : 1,         // This sets the initial panel
-			hashTags            : true,      // Should links change the hashtag in the URL?
-			
-			startText           : "Comenzar",   // Start button text
-			stopText            : "Frenar",    // Stop button text
-
-			
-			onSlideComplete : function(slider){
-				// alert('Welcome to Slide #' + slider.currentPage);
-			}
-});
-$('#slider2').anythingSlider({
-			startStopped    : false, // If autoPlay is on, this can force it to start stopped
-			width           : 550,   
-			height          : 200,   
-			theme           : 'metallic',
-			autoPlayLocked  : true,  // If true, user changing slides will not stop the slideshow
-			resumeDelay     : 10000, // Resume slideshow after user interaction, only if autoplayLocked is true (in milliseconds).
-			
-			startPanel          : 2,         // This sets the initial panel
-			hashTags            : true,      // Should links change the hashtag in the URL?
-			
-			startText           : "Comenzar",   // Start button text
-			stopText            : "Frenar",    // Stop button text
-
+			startStopped    : false,      // If autoPlay is on, this can force it to start stopped
+			width           : 550,        
+			height          : 200,        
+			theme           : 'metallic', 
+			autoPlayLocked  : true,       // If true, user changing slides will not stop the slideshow
+			resumeDelay     : 10000,      // Resume slideshow after user interaction, only if autoplayLocked is true (in milliseconds).
+			delay           : 5000,      // How long between slideshow transitions in AutoPlay mode (in milliseconds)
+			                              
+			startPanel      : 1,          // This sets the initial panel
+			hashTags        : true,       // Should links change the hashtag in the URL?
+			buildArrows     : false,      // If true, builds the forwards and backwards buttons
+			                              
+			startText       : "", // Start button text
+			stopText        : "",   // Stop button text
 			
 			onSlideComplete : function(slider){
 				// alert('Welcome to Slide #' + slider.currentPage);
