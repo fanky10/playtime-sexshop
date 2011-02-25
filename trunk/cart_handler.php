7f67744215aa334a575be98c333d16da87b57b36
fanky10@gmail.com <fanky10@gmail.com@a2532c14-442b-bbd0-053e-8828371a10ae>
2011-02-25 08:42:21 +0000

    ahora valida datos de entrada y muestra en una tablita pedorra los items x)

--- trunk/cart_handler.php
+++ trunk/cart_handler.php
@@ -1,39 +1,26 @@
 <?php
-session_start();
-//$d = (int) $_GET["destroy"];
-//if($d==1){
-//    session_unset();
-//}
+
 /**
  * este es el manejador del carrin!
  * basicamente va a haceptar tres parametros:
  * action [add | update | remove | empty]
  * id_producto (que se le hace la action)
- * cant (on update)
+ * cant (si no se especifica por fault es 1)
  */
+session_start();
 class ShoppingCart{
     //arreglo de items
     private $arrItems;
-    //para llevar la cuenta de cuantos items hay en carro :)
-    private $intItemsNum;
     public function __construct() {
-        $this->arrItems;//=Array();
-        $this->intItemsNum=0;
+        $this->arrItems=Array();
     }
     /*
      * agrego un nuevo id al arreglo
      * no sin antes fijarme si ya esta en el arreglo y sumarle la cantidad
      */
     function addItem($id_item,$cant){
-//        echo "adding item: $id_item, $cant";
         $arrLenght = count($this->arrItems);
-        //todo re cabeza
-//        echo "count $arrLenght";
-//        $this->arrItems[$arrLenght]['id']=$id_item;
-//        $this->arrItems[$arrLenght]['cant']=$cant;
-//        print_r($this->arrItems);
-//        echo "after add: ".count($this->arrItems);
-        //ahora me fijo si el producto ya existe e incremento su cant x)
+        //ahora me fijo si el producto ya existe e incremento su cant
         $found =0;//false
         for($index=0;$index < $arrLenght;$index++){
             $item = $this->arrItems[$index];
@@ -45,10 +32,6 @@ class ShoppingCart{
                 echo 'cant updated!';
             }
         }
-        //echo "this".$this->intItemsNum;// = $arrLenght;
-
-//        $this->intItemsNum++;
-//        $prod_idx=$prod_idx+1;
         if($found!=1){
             $this->arrItems[$arrLenght]['id']=$id_item;
             $this->arrItems[$arrLenght]['cant']=$cant;
@@ -59,12 +42,13 @@ class ShoppingCart{
         echo "found: $arrLenght <br>";
         print_r($this->arrItems);
     }
+    function getItems(){
+        return $this->arrItems;
+    }
 }
 
 //si la encontramos sin nada la creamos al toke
 if(!session_is_registered('cart')){//si no esta registrado lo registramos
-    echo "registrando...";
-    echo "<br>\n";
     $oCart = new ShoppingCart();
     $cart = serialize($oCart);
     $_SESSION['cart'] = $cart;
@@ -75,27 +59,59 @@ if($action=="clear"){
 }else if($action=="add"){
     $id_prod = (int)$_GET["prod_id"];
     $cant = (int)$_GET["qty"];
+    //validamos entradas
+    if($id_prod<0){
+        return;
+    }
+    if($cant < 0){
+        $cant = 0;
+    }
+    //obtenemos el carrito
     $s = $_SESSION['cart'];
     //lo transformamos en objeto
     $oCart = unserialize($s);
     $oCart->addItem($id_prod,$cant);
-    //ahora en bytes y lo subimos x)
+    //ahora en bytes y lo sobreescribimos
     $cart = serialize($oCart);
     $_SESSION['cart'] = $cart;
     echo "added!";
 }else if($action=="show"){
     $s = $_SESSION['cart'];
     $aCart = unserialize($s);
-    $aCart->show();
-//    echo "unserialized!: ".$aCart->getArray();
-//    print_r($aCart->getArray());
+    //lo mostramos un poquito mejor x)
+//    $aCart->show();
+    
+    ?>
+    <table border="1">
+        <tr>
+            <th>id_prod</th>
+            <th>cantidad</th>
+        </tr>
+<?php
+    //llamar a datos, pasarle el arreglo(de ids) y que me devuelva un array de productos
+    //con respectivas sus cants
+    $arrItems = $aCart->getItems();
+    $arrLenght = count($arrItems);
+
+    for($index=0;$index < $arrLenght;$index++){
+        $item = $arrItems[$index];
+        $current_id = $item['id'];
+        $current_cant = $item['cant'];
+        echo "<tr>";
+        echo "<td>".$current_id."</td>";
+        echo "<td>".$current_cant."</td>";
+        echo "<tr>";
+    }
 
 }
-
+?>
+    </table>
+<?php
 
 
 ?>
 <html>
+    <br>
     <a href="?action=clear">clear me</a>
     <br>
     <a href="?action=add&prod_id=1&qty=10">add me</a>
