9d0a8cfcf4d8d9b55df66ff35df6fccc7eaa4f05
fanky10@gmail.com <fanky10@gmail.com@a2532c14-442b-bbd0-053e-8828371a10ae>
2011-03-10 00:42:17 +0000

    modificacion nombre checkout.php a envio_checkout.php tareas modificadas producto_info ahora redirecciona correctamente

--- trunk/producto_info.php
+++ trunk/producto_info.php
@@ -24,10 +24,10 @@
         echo"<p class=\"prodInfo\">".$oProd->getDescription()."</p>";
 		?>
                 <!--aqui vamos a probar el cart_handler!-->
-        <form action="cart_handler.php?action=add&redirect=producto.php?id_prod=\".$oProd.\"" method="POST" id="formProducto">
+                <form action="cart_handler.php?action=add&redirect=producto.php?id_prod=<?php echo $oProd->getId_Producto()?>" method="POST" id="formProducto">
             <?php
             //ponemos un input hidden para que envie por POST el prod_id
-            echo "<input type=\"hidden\" name=\"prod_id\" value=\"$id_producto\" ";
+            echo "<input type=\"hidden\" name=\"prod_id\" value=\"$id_producto\"> ";
             ?>
 
             <div class="formField">
