32931d05a533ce281c26efe247fe33f5eadd5de3
Ewen Mackenzie <fanky10@gmail.com>
2011-03-12 14:00:51 +0000

    mail con comentario incluido (que se borro cuando marcelo paso a la revision 144 en vez de la 145)

--- factura.php
+++ factura.php
@@ -61,8 +61,9 @@
         	            <h1 class="categoria"><span>Confirmaci&oacute;n de compra</span></h1>
                             <?php //TODO: llamar a enviar email
                                 include_once 'entidades/email.php';
+                                $comment = $_POST["comentario"];
                                 $email = new Email();
-                                if($email->enviarEmail()){
+                                if($email->enviarEmail($comment)){
                             ?>
         	            <p class="copy">Esta misma factura de compra se le ha enviado a su direcci&oacute;n de correo electr&oacute;nico</p>
                             <?php
