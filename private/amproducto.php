3bad8085b82c50e38dbd9ca128d7fab918a28aa8
Ewen Mackenzie <fanky10@gmail.com>
2011-05-23 23:52:18 +0000

    trate de usar la capa de datos dentro de la carpeta private pero falle terriblemente... esperando por ayuda!

---
+++ private/amproducto.php
@@ -0,0 +1,93 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>Play Time Sex Shop .::. Botique Er&oacute;tica - CONTACTO</title>
+        <meta name="keywords" content="juguetes, toys, Lubricantes, Anillos, Estimuladores Clitoreales, Placer Anal, Doble Penetraci&oacute;n, Siliconados, Hot Pink, Black Fantasy, Realistic, Funny, Cyberskin, Desarrolladores Peneanos, extensiones, Arneses, Mu&ntilde;ecas, Vaginas, Disfraces" />
+        <meta name="description" content="Este es un sitio donde encontrar&aacute; muchas formas de dar placer a su pareja, formas de jugar y divertirse." />
+	<meta http-equiv="content-type" content="text/html;charset=iso-8859-1" />
+        
+            <!--CSS FILES-->
+        <link href="../css/style.css" rel="stylesheet" type="text/css" />
+
+            <!--JS FILES-->
+        <script src="../js/jquery-1.5.js" type="text/javascript"></script>
+        <script src="../js/secciones/build.js" type="text/javascript"></script><!--ANCHO COLUMNAS-->
+        <script src="../js/secciones/contact.js" type="text/javascript"></script><!--PROPIO-->
+        <script src="../js/jquery.validate.js" type="text/javascript"></script><!--EMAIL-->
+        <script type="text/javascript" src="../js/jquery.metadata.js"></script>
+        
+        <!--[if lte IE 7]>
+        <script type="text/javascript" src="js/supersleight-min.js"></script>
+        <![endif]-->
+        
+    </head>
+    
+    <body>
+        <div class="wrapper">
+            <div class="contenedor">
+                <div class="contenido">
+                    <div id="contenido_central">
+                        <div class="ruta"><a href="index.php">Inicio</a> / <a href="contacto.php">Contacto</a></div>
+
+                        <h1 class="categoria"><span>Alta Producto</span></h1>
+                        <p class="copy">Ingrese los datos de un nuevo producto.</p>
+                        
+                        <form id="frmContact">
+                            <fieldset style="display:none;"><input type="hidden" name="_method" value="POST" /></fieldset>
+
+                            <div class="formField">
+                            <label for="ContactName">Nombre Producto:</label>
+                            <input name="data[Contact][name]" type="text" class="inputData" maxlength="255" value="" id="ContactName" />                            
+                            </div>
+                            <!--
+                            <div class="formField">
+                                <?php
+                                //TODO: ver que onda con el tema de las marcas
+                                //include 'opc_marcas.php';
+                                ?>
+                            </div>
+                            -->
+                            <div class="formField">
+                                <?php
+                                include 'opc_categorias.php';
+                                ?>
+                            </div>
+                            <!--
+                            <div class="formField">
+                            <label for="ContactPhone">Tel&eacute;fono:</label>
+                            <input name="data[Contact][phone]" type="text" class="inputData" maxlength="255" value="" id="ContactPhone" />
+                            </div>
+                            <div class="formField">
+                            <label for="ContactRecipient">E-mail:</label>
+                            <input name="data[Contact][recipient]" type="text" class="inputData " value="" id="ContactRecipient" />
+                            </div>
+                            <div class="formField">
+                            <label for="ContactMessage">Comentario:</label>
+                            <textarea name="data[Contact][message]" cols="5" rows="3" class="textArea" id="ContactMessage" ></textarea>
+                            </div>
+                            -->
+                            <div class="formButton">
+                                <input type="submit" alt="Guardar" class="formButton" value="Guardar" />
+                            </div>
+                            <p></p>
+                        </form>
+                        <p id="mensaje"><strong>Los cambios se han efectuado correctamente!</strong></p>
+                        
+                    </div><!--end contenido_central-->
+
+                </div><!--end contenido -->
+
+            </div>
+			
+			
+			
+                <div class="pie_pagina">
+                        <a class="mawape" href="http://www.mawape.com.ar" title="MAWAPE Sistemas"><img src="images/logo-mawape.png" alt="MAWAPE Sistemas" title="MAWAPE Sistemas" /></a>
+                        <p class="copyright">&copy; Copyright 2011 - Play Time Sex Shop. Boutique Er&oacute;tica. Todos los derechos.</p>
+                </div><!--end pie-pagina-->
+		</div>
+    </body>
+</html>
+
