bbae4d45c6b79c0df572b5351e1164685d0a2a07
fanky10@gmail.com <fanky10@gmail.com@a2532c14-442b-bbd0-053e-8828371a10ae>
2011-08-03 23:17:13 +0000

    ahora las categorias pertenecientes a un producto en la modificacion, quedan seleccionadas (:

--- trunk/admin/opc_categorias.php
+++ trunk/admin/opc_categorias.php
@@ -4,12 +4,28 @@ include_once '../init.php';
 include_once ROOT_DIR .  '/datos/categorias.php';
 $dCateg = new DataCategorias();
 $vCat = $dCateg->getCategoriasHabilitadas();
+$id_producto = $_GET["id_producto"];
+if(isset ($id_producto)){
+    $dCateg = new DataCategorias();
+    $vCatProducto = $dCateg->getCategoriasProducto($id_producto);
+}
+
 echo "<label>Categoria:</label>";
-   echo "<select id=\"ProductCat\"  name=\"data[Product][category][]\" validate=\"required:true\" size=\"".(count($vCat)/2)."\" multiple=\"yes\">";
+   echo "<select id=\"ProductCat\"  name=\"data[Product][category][]\" validate=\"required:true\" size=\"9\" multiple=\"yes\">";
 //echo "count: ".count($vCat);
 for($index=0;$index < count($vCat);$index++){
     $oCat = $vCat[$index];
-    echo "<option value=\"".$oCat->getId_Categoria()."\">".$oCat->getNombre()."</option>";
+    $encontrado = false;
+    //busco sino bueno
+    if(isset($vCatProducto)){
+        for($j=0;$j < count($vCatProducto);$j++){
+            $ooCat = $vCatProducto[$j];
+            if($ooCat->getId_Categoria() == $oCat->getId_Categoria()){
+                $encontrado = true;
+            }
+        }
+    }
+    echo "<option value=\"".$oCat->getId_Categoria()."\" ".($encontrado?"selected":"")." >".$oCat->getNombre()." </option>\n";
 }
 ?>
 </select>
