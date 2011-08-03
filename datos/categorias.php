e7a06e35b17a2556f34a6e9ba0d640aa51ee2364
Ewen Mackenzie <fanky10@gmail.com>
2011-08-03 23:17:13 +0000

    ahora las categorias pertenecientes a un producto en la modificacion, quedan seleccionadas (:

--- datos/categorias.php
+++ datos/categorias.php
@@ -65,7 +65,7 @@ class DataCategorias extends Data{
         
 //        echo 'executing query: '.$query.'<br/>';
         $result = mysql_query($query)
-            or die ("getCategorias Query Failed: ".mysql_error()."<br/>");
+            or die ("getCategorias Query: $query </br> Failed: ".mysql_error()."<br/>");
         $cat_idx = 0;
         $vCategorias;
 
@@ -127,6 +127,29 @@ class DataCategorias extends Data{
 ////        echo "<br/>addCatProd: $query";
 //        $results = mysql_query($query)
 //            or die ("<br/>Query Failed ".mysql_error());
+    }
+    
+    /*
+     * retorna todas aquellas categorias habilitadas de un producto
+     */
+    public function getCategoriasProducto($id_producto){
+//        $query = "DROP TEMPORARY TABLE IF EXISTS tt_cats;";
+//        $result = mysql_query($query)
+//            or die ("refreshCategorias Query Failed <br/>query: $query <br/>".mysql_error());        
+//        $query = "CREATE TEMPORARY TABLE  tt_cats (
+//                SELECT cp.* FROM (select id_producto,max(fecha_hora) fecha_hora from categoria_producto WHERE id_producto = $id_producto group by id_producto) fp 
+//                INNER JOIN categoria_producto cp ON cp.id_producto = fp.id_producto and fp.fecha_hora = cp.fecha_hora
+//                );";
+//        $result = mysql_query($query)
+//            or die ("refreshCategorias Query Failed <br/>query: $query <br/>".mysql_error());
+        
+        $query = "SELECT c.id as id_categoria, nombre FROM categoria c INNER JOIN (
+                SELECT cp.* FROM (select id_producto,max(fecha_hora) fecha_hora from categoria_producto 
+                WHERE id_producto = $id_producto group by id_producto) fp 
+                INNER JOIN categoria_producto cp ON cp.id_producto = fp.id_producto and fp.fecha_hora = cp.fecha_hora
+                ) cp ON cp.id_categoria = c.id".
+            " WHERE c.habilitada=1";
+        return $this->getCategorias($query);
     }
     
 }
